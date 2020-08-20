d907dccdb20be92b045f7363eba3f490c195c1cb
TY LO DUNG <39002385+tylodung@users.noreply.github.com>
2020-08-20 08:33:40 +0700

    up

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`Con Đường Xưa Em Đi | ${post.frontmatter.title}`} />
+      <Helmet title={`Cuộc Đời | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
